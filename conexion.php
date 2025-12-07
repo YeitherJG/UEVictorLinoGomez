f9e15b66d358387dbd7f4559e1fb08deb6fe1d2b
YeitherJG <esojgabriel4@gmail.com>
2025-12-06 23:38:30 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
-$host = "db.vppyzoezwhuvdwhrbrdq.supabase.co";
-$port = "5432";
-$dbname = "postgres";
-$user = "postgres";
-$password = "Esojgabriel2003*";
+$host = getenv("DB_HOST");      // guarda tus credenciales en Render como variables de entorno
+$port = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
@@ -13,5 +13,3 @@ try {
     die("❌ Error de conexión: " . $e->getMessage());
 }
 ?>
-
-
