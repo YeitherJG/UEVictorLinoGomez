aefc4f8d1c054705ec9dc13ac2fbb37526eac450
YeitherJG <esojgabriel4@gmail.com>
2025-12-07 11:28:22 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,12 @@
 <?php
-$dsn = getenv("DATABASE_URL"); // la cadena completa del pooler session mode
+// Usa la cadena completa del pooler en una variable de entorno DATABASE_URL
+$dsn = getenv("DATABASE_URL"); 
+
 try {
-    $conexion = new PDO($dsn, null, null, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    echo "✅ Conexión exitosa usando pooler session mode!";
+    $conexion = new PDO($dsn, null, null, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "✅ Conexión exitosa a Supabase!";
 } catch (PDOException $e) {
     die("❌ Error de conexión: " . $e->getMessage());
 }
